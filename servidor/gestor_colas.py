8f47f1a08b90388cfd125a1d71c896612a7cc37c
marcoscassone02 <ma.cassone@alumno.um.edu.ar>
2025-07-15 10:57:04 -0300

    Cambio de prioridades en el gestor de colas

--- servidor/gestor_colas.py
+++ servidor/gestor_colas.py
@@ -1,16 +1,36 @@
 import queue
+import time
 
 class GestorColas:
     def __init__(self):
         self.cola = queue.PriorityQueue()
-        self.contador = 0  # Para desempatar turnos con misma prioridad
+        self.contador = 0  
 
     def agregar_turno(self, prioridad, tramite, cliente_id, nombre, conn, addr):
         self.contador += 1
-        # Guardar también el nombre, la conexión y la dirección
-        self.cola.put((prioridad, self.contador, tramite, cliente_id, nombre, conn, addr))
+        timestamp = time.time()
+        self.cola.put((prioridad, self.contador, tramite, cliente_id, nombre, conn, addr, timestamp))
 
     def obtener_turno(self):
+        if self.cola.empty():
+            return None
+        turnos = []
+        while not self.cola.empty():
+            turnos.append(self.cola.get())
+        now = time.time()
+        turnos_actualizados = []
+        for t in turnos:
+            prioridad, contador, tramite, cliente_id, nombre, conn, addr, timestamp = t
+            espera = now - timestamp
+            if espera >= 30 and prioridad > 1:
+                prioridad -= 1
+                
+                timestamp = now
+            turnos_actualizados.append((prioridad, contador, tramite, cliente_id, nombre, conn, addr, timestamp))
+       
+        for t in turnos_actualizados:
+            self.cola.put(t)
+        
         if not self.cola.empty():
             return self.cola.get()
         return None
